5245815ade54f2652161c9df6595871a156a2818
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-01-12 13:20:18 +0500

    feat: bcrypt added for password encryption

--- features/user/user.entity.ts
+++ features/user/user.entity.ts
@@ -1,5 +1,6 @@
 import { Schema, model } from "mongoose";
 import { isEmail } from "validator";
+import { genSalt, hash } from "bcrypt";
 
 const userSchema = new Schema({
   firstName: {
@@ -21,6 +22,14 @@ const userSchema = new Schema({
     required: [true, "password is required, but recived an empty value"],
     minlength: [3, "minimun password length is 6 characters"],
   },
+});
+
+// Mongoose hook
+// this hook fires before any thing is saved in user collection in db
+userSchema.pre("save", async function (next) {
+  const salt = await genSalt();
+  this.password = await hash(this.password, salt);
+  next();
 });
 
 export const User = model("user", userSchema);
